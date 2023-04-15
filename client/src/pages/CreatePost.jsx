ae95d892f7be1872bfe139a48ab18165d18bdbbd
PabloRosscobar <58635592+PabloRosscobar@users.noreply.github.com>
2023-04-15 15:24:47 +0100

    Endpoint fix

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -54,13 +54,16 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify(form),
-        });
+        const response = await fetch(
+          "https://dall-e-z3rp.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(form),
+          }
+        );
 
         await response.json();
 
